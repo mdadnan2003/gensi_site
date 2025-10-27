257ca11242f02908ab7705ab1fda668e97545a5a
mdadnan2003 <mdadnaniftekhari@gmail.com>
2025-10-27 17:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')  # templates/index.html render karega
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000), debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
